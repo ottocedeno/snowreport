5fff5e2ab6505f6e79fd9fe927120d3ed425d2b6
Otto Cedeno <otto@ottostacos.com>
2020-01-10 03:20:52 +0000

    updated instructions method

--- lib/SnowReport/CLI.rb
+++ lib/SnowReport/CLI.rb
@@ -20,14 +20,18 @@ class SnowReport::CLI
     puts ""
     puts "* * *    * *    * * *    * *    * * *    * *    * * *    * *    * * *    * * "
     puts " * *    * * *    * *    * * *    * *    * * *    * *    * * *    * *    * * *"
-    puts ""
+
   end
 
   def instructions
+    puts ""
+    puts "INSTRUCTIONS"
     puts "Snow Report accepts the following commands:"
-    puts "  TOP MOUNTAINS:  Shows a list of the top X mountains by snowfall... \n    To select a specific mountain from the list, type it's NAME"
+    puts "  TOP MOUNTAINS:  Shows a list of the top X mountains by snowfall..."
+    puts "    To select a specific mountain from the list, type it's NAME"
     puts "  HELP:  Will repeat this message at anytime"
     puts "  EXIT:  Will exit the application"
+    puts ""
   end
 
 end
