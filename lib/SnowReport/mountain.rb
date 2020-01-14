8a9ef5c46d6f35a30bf2c3ffbb2a7b17fe4209f3
Otto Cedeno <otto@ottostacos.com>
2020-01-14 04:41:25 +0000

    Added .all and #save methods for Mountain class

--- lib/SnowReport/mountain.rb
+++ lib/SnowReport/mountain.rb
@@ -3,9 +3,22 @@ require 'colorize'
 class SnowReport::Mountain
   attr_accessor :name, :snow_depth, :mountain_url, :region, :current_temp, :last_snowfall, :trails_open, :lifts_open
 
-  def initialize(name)
-    @name = name
+  @@all = []
+
+  def initialize
+    save
   end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
 end
 
-mam = SnowReport::Mountain.new("Mammouth")
+
+#for testing...
+mam = SnowReport::Mountain.new
