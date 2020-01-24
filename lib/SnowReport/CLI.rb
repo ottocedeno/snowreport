b70296c3109dfcd2a4436778a20014c1656aa756
Otto Cedeno <otto@ottostacos.com>
2020-01-24 01:01:14 +0000

    added comments to CLI class

--- lib/SnowReport/CLI.rb
+++ lib/SnowReport/CLI.rb
@@ -2,6 +2,7 @@ require 'colorize'
 
 class SnowReport::CLI
 
+  #Responsible for the high level application logic:
   def start
     welcome
     load_mountain_data
@@ -9,6 +10,7 @@ class SnowReport::CLI
     decision_loop
   end
 
+  #Responsible for introduction to the user:
   def welcome
     puts ""
     puts "* * * *   * * * *".colorize(:light_white)
@@ -22,25 +24,16 @@ class SnowReport::CLI
     puts "Snow Report shows you which mountains are expected to get DUMPED on over the next few days."
     puts "You can even select mountains from the list to find out more info!"
     puts "Ride Responsibly!".upcase
-  end
-
-  def instructions
-    puts ""
-    puts "INSTRUCTIONS".colorize(:blue).bold
-    puts "Snow Report accepts the following commands:"
-    puts "  TOP TEN:  ".colorize(:red) + "Will show you a list of which mountains are expecting the most snow in the coming days."
-    puts "  Type a mountain's " + "NAME:".colorize(:red) + "Will show you more details on that specific mountain."
-    puts "  REFRESH:  ".colorize(:red) + "Will update mountain data"
-    puts "  HELP:  ".colorize(:red) + "Will repeat this message at anytime"
-    puts "  EXIT:  ".colorize(:red) + "Will exit the application"
     puts ""
   end
 
+  #Responsible for calling appropriate Mountain object methods that loads mountain data into the application:
   def load_mountain_data
-    puts ""
     puts "Loading Mountain Data...".upcase.colorize(:red).bold
-    puts ""
+
+    #This step is important if the user REFRESHES:
     SnowReport::Mountain.reset if SnowReport::Mountain.all.size > 0
+
     SnowReport::Mountain.new_from_mountains_array(SnowReport::Scraper.download_opensnow_index)
     SnowReport::Mountain.update_all_mountains
 
@@ -48,18 +41,32 @@ class SnowReport::CLI
     puts ""
   end
 
+  #Responsible for educating the user on available commands:
+  def instructions
+    puts "INSTRUCTIONS".colorize(:blue).bold
+    puts "Snow Report accepts the following commands:"
+    puts "  TOP TEN:  ".colorize(:red) + "Will show you a list of which mountains are expecting the most snow in the coming days."
+    puts "  Type a mountain's " + "NAME:  ".colorize(:red) + "Will show you more details on that specific mountain."
+    puts "  REFRESH:  ".colorize(:red) + "Will update mountain data"
+    puts "  HELP:  ".colorize(:red) + "Will repeat this message at anytime"
+    puts "  EXIT:  ".colorize(:red) + "Will exit the application"
+    puts ""
+  end
+
+  # Responsible for prompting user for input and returning it's value:
   def prompt
     print "What would you like to do?  "
     gets.strip.downcase
   end
 
+  #Responsible for letting the user know of an invalid command:
   def input_error
-    puts ""
     puts "Wipe Out! That command was not found.".upcase.colorize(:red).bold
     puts "Try HELP if you're stuck.".colorize(:red)
     puts ""
   end
 
+  #Responsible for the main interactive application loop and returning the appropriate data depending on user input:
   def decision_loop
     user_input = nil
 
